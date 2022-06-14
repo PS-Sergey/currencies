e7e01f229913daecb9eb2f26de6cd077db4c5e61
Polyakov Sergey <sergey.polyakov.1996@gmail.com>
2022-06-14 21:30:21 +0400

    Refactor imports

--- src/main/java/com/polyakov/currencies/service/OpenExchangeRatesService.java
+++ src/main/java/com/polyakov/currencies/service/OpenExchangeRatesService.java
@@ -1,6 +1,5 @@
 package com.polyakov.currencies.service;
 
-import java.util.List;
 import java.util.Map;
 
 public interface OpenExchangeRatesService {
--- src/main/java/com/polyakov/currencies/service/OpenExchangeRatesServiceImpl.java
+++ src/main/java/com/polyakov/currencies/service/OpenExchangeRatesServiceImpl.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.Map;
 
 @Service
