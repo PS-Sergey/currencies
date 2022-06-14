77dc915323d54318f9a97feffec4cc7464e3e441
polyakov <polyakov@o-code.ru>
2022-06-14 17:39:24 +0400

    Add tests

---
+++ src/test/java/com/polyakov/currencies/controller/CurrencyControllerTest.java
@@ -0,0 +1,60 @@
+package com.polyakov.currencies.controller;
+
+import com.polyakov.currencies.service.GiphyService;
+import com.polyakov.currencies.service.OpenExchangeRatesService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(CurrencyController.class)
+class CurrencyControllerTest {
+
+    @Value("${giphy.rich}")
+    private String rich;
+
+    @MockBean
+    private OpenExchangeRatesService openExchangeRatesService;
+
+    @MockBean
+    private GiphyService giphyService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void whenGetCurrenciesThenReturnJsonResult() throws Exception {
+        Map<String, String> responseMap = new HashMap<>();
+        responseMap.put("RUB", "Russian Ruble");
+        String expectingResult = "{\"RUB\":\"Russian Ruble\"}";
+        Mockito.when(openExchangeRatesService.getCurrencies())
+            .thenReturn(responseMap);
+        mockMvc.perform(get("/api/currencies"))
+            .andDo(print())
+            .andExpect(content().json(expectingResult));
+    }
+
+    @Test
+    void whenGetRateThenReturnStatusOkAndContentType() throws Exception {
+        Mockito.when(openExchangeRatesService.compareRate())
+            .thenReturn(1);
+        Mockito.when(giphyService.getGif(rich))
+            .thenReturn(new byte[1]);
+        mockMvc.perform(get("/api/rate"))
+            .andDo(print())
+            .andExpect(content().contentType(MediaType.IMAGE_GIF))
+            .andExpect(status().isOk());
+    }
+}
---
+++ src/test/java/com/polyakov/currencies/service/GiphyServiceImplTest.java
@@ -0,0 +1,52 @@
+package com.polyakov.currencies.service;
+
+import com.polyakov.currencies.feign.GiphyFeign;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.*;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+
+@SpringBootTest
+class GiphyServiceImplTest {
+
+    @MockBean
+    private GiphyFeign giphyFeign;
+
+    @MockBean
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private GiphyServiceImpl giphyService;
+
+    @Test
+    void whenGetGifThenReturnByteArray() throws IOException {
+        InputStream resource = new ClassPathResource("parseTest").getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(resource));
+        String giphyFeignResponse = reader
+                                        .lines()
+                                        .collect(Collectors.joining("\n"));
+        String gifUrl = "TestUrl";
+        RequestEntity requestEntity = RequestEntity
+                                          .get(gifUrl)
+                                          .build();
+        byte[] gif = new byte[1];
+
+        Mockito.when(giphyFeign.getGif(anyString(), anyString()))
+            .thenReturn(giphyFeignResponse);
+        Mockito.when(restTemplate.exchange(requestEntity, byte[].class))
+            .thenReturn(ResponseEntity.ok().body(gif));
+        byte[] result = giphyService.getGif("testTag");
+        assertEquals(gif, result);
+    }
+}
